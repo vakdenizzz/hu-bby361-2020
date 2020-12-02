3cbdccf0da8eac7ab63500ab90a9adba81ed380e
vakdenizzz <48294816+vakdenizzz@users.noreply.github.com>
2020-12-02 14:56:07 +0300

    Update VolkanAkdeniz_02.sql

--- VolkanAkdeniz_02.sql
+++ VolkanAkdeniz_02.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 28 Kas 2020, 12:48:46
+-- Üretim Zamanı: 02 Ara 2020, 12:53:54
 -- Sunucu sürümü: 10.4.14-MariaDB
 -- PHP Sürümü: 7.4.10
 
@@ -43,7 +43,7 @@ CREATE TABLE `alanbirlestirme` (
 CREATE TABLE `bagisbilgisi` (
   `BagısNo` int(11) NOT NULL COMMENT 'Kurum veya kisi  bilgilerine ait numara sırayla ve otomatik bir sekilde artıs gösterecek.\r\n',
   `BagısTipi` varchar(99) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Bagış yapan kurum veya kisi ismi ve  özel mi yoksa kurum mu bagış yapıyor bilgisi girilecek\r\n',
-  `BagısAdı` varchar(99) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Bagış yapan kurum veya kisinin, ismi girilecek.\r\n',
+  `BagısAdı` varchar(99) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Bagış yapan kurum veya kisinin, adı girilecek.\r\n',
   `BagısEPosta` varchar(99) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Bagış yapan kurum veya kisinin kurumsal iletisim icin kullandığı mail adresi girilecek.\r\n'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -52,16 +52,27 @@ CREATE TABLE `bagisbilgisi` (
 --
 
 INSERT INTO `bagisbilgisi` (`BagısNo`, `BagısTipi`, `BagısAdı`, `BagısEPosta`) VALUES
-(1, 'kisi bagısı', 'Volkan Akdeniz', 'volkan_98fb@gmail.com'),
-(2, 'kisi', 'Ahmet Dursun', 'ahmett99@gmail.com'),
-(3, 'özel', 'hacettepe kütüphanesi', 'hacettepete@edu.tr.com'),
-(4, 'ÖzelBagıs', 'Bilkent Üniversitesi', 'bilkent@edu.tr.com'),
-(5, 'Özel', 'Çankaya Üniversitesi', 'cankaya@edu.tr.com'),
+(1, 'Kisi', 'Volkan Akdeniz', 'volkan_98fb@gmail.com'),
+(2, 'Kisi', 'Ahmet Dursun', 'ahmett99@gmail.com'),
+(3, 'Özel', 'Hacettepe kütüphanesi', 'hacettepete@edu.tr.com'),
+(4, 'Özel', 'Bilkent Üniversitesi', 'bilkent@edu.tr.com'),
+(5, 'Özel', 'Cankaya Üniversitesi', 'cankaya@edu.tr.com'),
 (6, 'Özel', 'Bilkent Üniversitesi', 'bilkent@edu.tr.com'),
-(7, 'Kişi', 'Cemal Can', 'cemallaydın@gmail.com'),
-(8, 'Özel', 'Çankaya Üniversitesi', 'cankaya@edu.tr.com'),
-(9, 'Kişi', 'Volkan Akdeniz', 'v.akdenizzz@gmail.com'),
-(10, 'Kişi', 'Volkan Akdeniz', 'v.akdenizzz@gmail.com');
+(7, 'Kisi', 'Cemal Can', 'cemallaydın@gmail.com'),
+(8, 'Özel', 'Cankaya Üniversitesi', 'cankaya@edu.tr.com'),
+(9, 'Kisi', 'Volkan Akdeniz', 'v.akdenizzz@gmail.com'),
+(10, 'Kisi', 'Volkan Akdeniz', 'v.akdenizzz@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Görünüm yapısı durumu `derecebir`
+-- (Asıl görünüm için aşağıya bakın)
+--
+CREATE TABLE `derecebir` (
+`KitapAdı` varchar(99)
+,`KitapAdetBilgisi` int(99)
+);
 
 -- --------------------------------------------------------
 
@@ -76,38 +87,6 @@ CREATE TABLE `dörtkitaptanfazla` (
 ,`KitapAdı` varchar(99)
 ,`KitapAdetBilgisi` int(99)
 ,`DilBilgisi` varchar(99)
-);
-
--- --------------------------------------------------------
-
---
--- Görünüm yapısı durumu `fullekran`
--- (Asıl görünüm için aşağıya bakın)
---
-CREATE TABLE `fullekran` (
-`BagısTipi` varchar(99)
-,`BagısAdı` varchar(99)
-,`BagısEPosta` varchar(99)
-,`ISBN` varchar(13)
-,`DilBilgisi` varchar(99)
-,`KitapAdetBilgisi` int(99)
-,`KitapAdı` varchar(99)
-,`YazarAdı` varchar(99)
-,`YayıneviAdı` varchar(98)
-,`YayıneviEPosta` varchar(98)
-,`YayıneviUrl` varchar(98)
-);
-
--- --------------------------------------------------------
-
---
--- Görünüm yapısı durumu `genelgörünüm`
--- (Asıl görünüm için aşağıya bakın)
---
-CREATE TABLE `genelgörünüm` (
-`KitapAdı` varchar(99)
-,`YazarAdı` varchar(99)
-,`YayıneviAdı` varchar(98)
 );
 
 -- --------------------------------------------------------
@@ -131,38 +110,46 @@ CREATE TABLE `kitapbilgileri` (
 
 INSERT INTO `kitapbilgileri` (`KitapKayıtNo`, `ISBN`, `KitapYayımTarihi`, `KitapAdı`, `KitapAdetBilgisi`, `DilBilgisi`) VALUES
 (1, '1234567899876', 2020, 'Kürk Mantolu Madonna', 1, 'Türkçe'),
-(2, '2234567899879', 2020, 'Hayvan ciftligi', 0, 'Türkçe'),
+(2, '2234567899879', 2020, 'Hayvan Ciftligi', 0, 'Türkçe'),
 (3, '4334567899856', 2020, 'Körlük', 1, 'Türkçe'),
 (4, '3634567899645', 2020, 'Fareler ve İnsanlar', 2, 'Türkçe'),
-(5, '7534567899893', 2020, 'Şeker Portakalı', 4, 'Türkçe'),
+(5, '7534567899893', 2020, 'Seker Portakalı', 4, 'Türkçe'),
 (6, '2734567899817', 2020, 'Simyacı', 8, 'Türkçe'),
 (7, '983456789665', 2018, 'Cesur Yeni Dünya', 3, 'İngilizce'),
-(8, '7434567897483', 2020, 'Başkalarının Hayatı', 5, 'İngilizce'),
+(8, '7434567897483', 2020, 'Baskalarının Hayatı', 5, 'İngilizce'),
 (9, '6634567893254', 2020, 'Kız Babası ve Kedi', 2, 'İngilizce'),
 (10, '6934567899833', 2020, 'Madeline Miller', 7, 'İngilizce');
 
 -- --------------------------------------------------------
 
 --
--- Görünüm yapısı durumu `tamliste`
+-- Görünüm yapısı durumu `tablo1`
 -- (Asıl görünüm için aşağıya bakın)
 --
-CREATE TABLE `tamliste` (
-`BagısAdı` varchar(99)
-,`KitapAdı` varchar(99)
+CREATE TABLE `tablo1` (
+`KitapAdı` varchar(99)
 ,`KitapAdetBilgisi` int(99)
+,`DilBilgisi` varchar(99)
+,`BagısTipi` varchar(99)
+,`BagısAdı` varchar(99)
 ,`YayıneviAdı` varchar(98)
-,`YazarAdı` varchar(99)
+,`YayıneviUrl` varchar(98)
+,`AD_SOYAD` varchar(199)
 );
 
 -- --------------------------------------------------------
 
 --
--- Görünüm yapısı durumu `teklisorgu`
+-- Görünüm yapısı durumu `tablo2`
 -- (Asıl görünüm için aşağıya bakın)
 --
-CREATE TABLE `teklisorgu` (
-`DilBilgisi` varchar(99)
+CREATE TABLE `tablo2` (
+`KitapAdı` varchar(99)
+,`KitapAdetBilgisi` int(99)
+,`DilBilgisi` varchar(99)
+,`YayıneviAdı` varchar(98)
+,`YayıneviUrl` varchar(98)
+,`AD_SOYAD` varchar(199)
 );
 
 -- --------------------------------------------------------
@@ -190,10 +177,10 @@ INSERT INTO `yayinevi` (`YayıneviNo`, `YayıneviKayıtTarihi`, `YayıneviAdı`,
 (4, '2020-10-15', 'Sel Yayıncılık', 'https://www.selyayincilik.com/', 'destek@selyayincilik.com'),
 (5, '2020-11-03', ' Can Çocuk Yayınları', 'https://canyayinlari.com/', 'dosya@canyayinlari.com '),
 (6, '2020-11-01', 'Can Yayınları', 'https://canyayinlari.com/', 'dosya@canyayinlari.com '),
-(7, '2020-09-08', 'İthaki Yayınları', 'http://www.ithaki.com.tr/', 'info@ithaki.com.tr'),
-(8, '2020-08-04', 'Beyaz Baykuş', 'https://beyazbaykus.com/', 'info@beyazbaykus.com'),
+(7, '2020-09-08', 'Ithaki Yayınları', 'http://www.ithaki.com.tr/', 'info@ithaki.com.tr'),
+(8, '2020-08-04', 'Beyaz Baykus', 'https://beyazbaykus.com/', 'info@beyazbaykus.com'),
 (9, '2020-07-14', 'Destek Yayınları', 'https://destekdukkan.com/anasayfa', 'info@destekyayinlari.com'),
-(10, '2020-08-20', 'İthaki Yayın', 'http://www.ithaki.com.tr/', '	\r\ninfo@ithaki.com.tr');
+(10, '2020-08-20', 'Ithaki Yayın', 'http://www.ithaki.com.tr/', '	\r\ninfo@ithaki.com.tr');
 
 -- --------------------------------------------------------
 
@@ -203,7 +190,8 @@ INSERT INTO `yayinevi` (`YayıneviNo`, `YayıneviKayıtTarihi`, `YayıneviAdı`,
 
 CREATE TABLE `yazarbilgileri` (
   `YazarNo` int(11) NOT NULL COMMENT 'Yazar bilgilerine ait numaralar sırayla ve otomatik bir sekilde artıs gösterecek.\r\n',
-  `YazarAdı` varchar(99) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Yazarın isim ve soyisim bilgileri girilecek.\r\n',
+  `YazarAdi` varchar(99) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Yazarın isim ve soyisim bilgileri girilecek.\r\n',
+  `YazarSoyad` varchar(99) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Yazar Soyadı bilgisi girilecek.',
   `YazarDoğum Tarihi` date DEFAULT current_timestamp() COMMENT 'Yazarın dogum tarihi sisteme girilecektir.\r\n',
   `YazarURL` varchar(99) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Yazara ait bilgilere Yönlendirir.\r\n'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -212,17 +200,17 @@ CREATE TABLE `yazarbilgileri` (
 -- Tablo döküm verisi `yazarbilgileri`
 --
 
-INSERT INTO `yazarbilgileri` (`YazarNo`, `YazarAdı`, `YazarDoğum Tarihi`, `YazarURL`) VALUES
-(1, 'Sabahhatin Ali', '2020-10-28', NULL),
-(2, 'George Orwell', '1988-08-04', NULL),
-(3, 'Jose Saramago', '1990-11-09', NULL),
-(4, 'John Steinbeck', '1988-11-13', NULL),
-(5, 'Jose Mauro De Vasconcelos', '1920-10-20', NULL),
-(6, 'Paulo Coelho', '2000-01-07', NULL),
-(7, 'Aldous Huxley', '1987-04-08', NULL),
-(8, 'Amy Grace Loyd', '1988-07-08', NULL),
-(9, 'Erol Hızarcı', '1986-09-10', NULL),
-(10, 'Madeline Miller', '1996-06-09', NULL);
+INSERT INTO `yazarbilgileri` (`YazarNo`, `YazarAdi`, `YazarSoyad`, `YazarDoğum Tarihi`, `YazarURL`) VALUES
+(1, 'Sabahhatin Ali', 'Senyuva', '2020-10-28', NULL),
+(2, 'Orwell', 'George ', '1988-08-04', NULL),
+(3, 'Saramago', 'Jose ', '1990-11-09', NULL),
+(4, 'Steinbeck', 'John', '1988-11-13', NULL),
+(5, 'Vasconcelos', 'Jose Mauro', '1920-10-20', NULL),
+(6, 'Coelho', 'Paulo ', '2000-01-07', NULL),
+(7, 'Huxley', 'Aldous', '1987-04-08', NULL),
+(8, 'Grace Loyd', 'Amy ', '1988-07-08', NULL),
+(9, 'Hızarcı', 'Erol ', '1986-09-10', NULL),
+(10, 'Miller', 'Madeline', '1996-06-09', NULL);
 
 -- --------------------------------------------------------
 
@@ -236,6 +224,15 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- --------------------------------------------------------
 
 --
+-- Görünüm yapısı `derecebir`
+--
+DROP TABLE IF EXISTS `derecebir`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `derecebir`  AS  select `k`.`KitapAdı` AS `KitapAdı`,`k`.`KitapAdetBilgisi` AS `KitapAdetBilgisi` from `kitapbilgileri` `k` where `k`.`KitapAdetBilgisi` < 6 ;
+
+-- --------------------------------------------------------
+
+--
 -- Görünüm yapısı `dörtkitaptanfazla`
 --
 DROP TABLE IF EXISTS `dörtkitaptanfazla`;
@@ -245,38 +242,20 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- --------------------------------------------------------
 
 --
--- Görünüm yapısı `fullekran`
+-- Görünüm yapısı `tablo1`
 --
-DROP TABLE IF EXISTS `fullekran`;
+DROP TABLE IF EXISTS `tablo1`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `fullekran`  AS  select `b`.`BagısTipi` AS `BagısTipi`,`b`.`BagısAdı` AS `BagısAdı`,`b`.`BagısEPosta` AS `BagısEPosta`,`k`.`ISBN` AS `ISBN`,`k`.`DilBilgisi` AS `DilBilgisi`,`k`.`KitapAdetBilgisi` AS `KitapAdetBilgisi`,`k`.`KitapAdı` AS `KitapAdı`,`y`.`YazarAdı` AS `YazarAdı`,`e`.`YayıneviAdı` AS `YayıneviAdı`,`e`.`YayıneviEPosta` AS `YayıneviEPosta`,`e`.`YayıneviUrl` AS `YayıneviUrl` from (((`bagisbilgisi` `b` join `kitapbilgileri` `k`) join `yazarbilgileri` `y`) join `yayinevi` `e`) where `b`.`BagısNo` = `k`.`KitapKayıtNo` and `b`.`BagısNo` = `y`.`YazarNo` and `b`.`BagısNo` = `e`.`YayıneviNo` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `tablo1`  AS  select `k`.`KitapAdı` AS `KitapAdı`,`k`.`KitapAdetBilgisi` AS `KitapAdetBilgisi`,`k`.`DilBilgisi` AS `DilBilgisi`,`b`.`BagısTipi` AS `BagısTipi`,`b`.`BagısAdı` AS `BagısAdı`,`e`.`YayıneviAdı` AS `YayıneviAdı`,`e`.`YayıneviUrl` AS `YayıneviUrl`,concat(`y`.`YazarAdi`,' ',`y`.`YazarSoyad`) AS `AD_SOYAD` from (((`bagisbilgisi` `b` join `kitapbilgileri` `k`) join `yayinevi` `e`) join `yazarbilgileri` `y`) where `b`.`BagısNo` = `k`.`KitapKayıtNo` and `b`.`BagısNo` = `e`.`YayıneviNo` and `b`.`BagısNo` = `y`.`YazarNo` ;
 
 -- --------------------------------------------------------
 
 --
--- Görünüm yapısı `genelgörünüm`
+-- Görünüm yapısı `tablo2`
 --
-DROP TABLE IF EXISTS `genelgörünüm`;
+DROP TABLE IF EXISTS `tablo2`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `genelgörünüm`  AS  select `k`.`KitapAdı` AS `KitapAdı`,`y`.`YazarAdı` AS `YazarAdı`,`e`.`YayıneviAdı` AS `YayıneviAdı` from ((`yazarbilgileri` `y` join `yayinevi` `e`) join `kitapbilgileri` `k`) where `y`.`YazarNo` = `e`.`YayıneviNo` and `y`.`YazarNo` = `k`.`KitapKayıtNo` ;
-
--- --------------------------------------------------------
-
---
--- Görünüm yapısı `tamliste`
---
-DROP TABLE IF EXISTS `tamliste`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `tamliste`  AS  select `b`.`BagısAdı` AS `BagısAdı`,`k`.`KitapAdı` AS `KitapAdı`,`k`.`KitapAdetBilgisi` AS `KitapAdetBilgisi`,`e`.`YayıneviAdı` AS `YayıneviAdı`,`y`.`YazarAdı` AS `YazarAdı` from (((`bagisbilgisi` `b` join `kitapbilgileri` `k`) join `yayinevi` `e`) join `yazarbilgileri` `y`) where `b`.`BagısNo` = `k`.`KitapKayıtNo` and `b`.`BagısNo` = `e`.`YayıneviNo` and `b`.`BagısNo` = `y`.`YazarNo` ;
-
--- --------------------------------------------------------
-
---
--- Görünüm yapısı `teklisorgu`
---
-DROP TABLE IF EXISTS `teklisorgu`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `teklisorgu`  AS  select `kitapbilgileri`.`DilBilgisi` AS `DilBilgisi` from `kitapbilgileri` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `tablo2`  AS  select `k`.`KitapAdı` AS `KitapAdı`,`k`.`KitapAdetBilgisi` AS `KitapAdetBilgisi`,`k`.`DilBilgisi` AS `DilBilgisi`,`e`.`YayıneviAdı` AS `YayıneviAdı`,`e`.`YayıneviUrl` AS `YayıneviUrl`,concat(`y`.`YazarAdi`,' ',`y`.`YazarSoyad`) AS `AD_SOYAD` from ((`kitapbilgileri` `k` join `yayinevi` `e`) join `yazarbilgileri` `y`) where `k`.`KitapKayıtNo` = `e`.`YayıneviNo` and `k`.`KitapKayıtNo` = `y`.`YazarNo` ;
 
 --
 -- Dökümü yapılmış tablolar için indeksler
